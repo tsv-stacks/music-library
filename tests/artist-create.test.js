44151efcaf0c684a3369b349022e2ce43649107f
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-02-27 22:12:05 +0000

    setup complete for second test

--- tests/artist-create.test.js
+++ tests/artist-create.test.js
@@ -9,18 +9,18 @@ describe('create artist', () => {
       it('creates a new artist in the database', async () => {
         const { status, body } = await request(app).post('/artists').send({
           name: 'Tame Impala',
-          genre: 'rock',
+          genre: 'indie',
         });
 
         expect(status).to.equal(201);
         expect(body.name).to.equal('Tame Impala');
-        expect(body.genre).to.equal('rock');
+        expect(body.genre).to.equal('indie');
 
         const {
           rows: [artistData],
         } = await db.query(`SELECT * FROM Artists WHERE id = ${body.id}`);
         expect(artistData.name).to.equal('Tame Impala');
-        expect(artistData.genre).to.equal('rock');
+        expect(artistData.genre).to.equal('indie');
       });
     });
   });
