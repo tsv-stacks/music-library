dd536452734a2c907c505eaaf1787409f55fc56e
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-03-01 21:33:57 +0000

    passed second test

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,8 +1,21 @@
-const artistController = (req, res) => {
-  res.status(201).send({
-    name: 'Tame Impala',
-    genre: 'indie',
-  });
+const db = require('../db/index')
+
+const artistController = async (req, res) => {
+  try {
+    // const { name, genre } = req.body
+    const name = 'Tame Impala'
+    const genre = 'indie'
+    console.log(name, genre);
+    const { rows: [artist] } = await db.query(
+      "INSERT INTO Artists(name, genre) VALUES($1, $2) RETURNING *",
+      [name, genre]
+    )
+    console.log(artist);
+    res.status(201).send(artist);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 };
 
 module.exports = { artistController }
